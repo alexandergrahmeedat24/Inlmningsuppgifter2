04375a1901b4a75fa99fc64f8195588a9b60ed78
hmarhamza <algr08001@utb.vaxjo.se>
2025-11-28 08:22:37 +0100

    Add project files.

---
+++ Uppgifter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgifter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int age = int.Parse(Console.ReadLine());
+            {
+                if (age == 16)
+                    Console.WriteLine("Du får vara med i tävlingen.");
+                else if (age == 17)
+                    Console.WriteLine("Du får vara med i tävlingen.");
+                else if (age == 18)
+                    Console.WriteLine("Du får vara med i tävlingen.");
+                else if (age == 19)
+                    Console.WriteLine("Du får vara med i tävlingen.");
+                else
+                    Console.WriteLine("Du får tyvärr inte vara med i tävlingen");
+            }
+            
+        }
+    }
+}
